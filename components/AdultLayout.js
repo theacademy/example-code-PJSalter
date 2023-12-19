ca425e13b471e2f1ddcfdb6a3cc8164bf417264c
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-12-19 20:29:20 +0000

    removed project into it's own folder

--- components/AdultLayout.js
+++
@@ -1,3 +0,0 @@
-export default function AdultLayout({ children }) {
-  return <div style={{ fontFamily: "Arial, sans-serif" }}>{children}</div>;
-}
--- components/Styled-Components/AdultForm.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const StyledForm = styled.form`
-  font-size: 1.5rem;
-  text-align: center;
-`;
-
-export default StyledForm;
